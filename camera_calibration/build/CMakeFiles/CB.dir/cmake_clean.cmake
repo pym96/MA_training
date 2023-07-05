f247e6ecc26cd4121c1e557a1673df4d90250bc0
pym96 <pym66@outlook.com>
2023-07-05 10:44:25 +1000

    Add a custom camera calibration code

---
+++ camera_calibration/build/CMakeFiles/CB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CB"
+  "CB.pdb"
+  "CMakeFiles/CB.dir/camera_caliberation.cpp.o"
+  "CMakeFiles/CB.dir/camera_caliberation.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ camera_calibration/build/CMakeFiles/CB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CB.
+# This may be replaced when dependencies are built.
